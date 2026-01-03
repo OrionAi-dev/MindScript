4d3843b527c28735a317e1dd7251a336b3493c9f
OrionSoul <orion@orionsoul.com>
2026-01-03 15:56:58 -0500

    added events

---
+++ packages/openspec-events/dist/index.cjs
@@ -0,0 +1,47 @@
+"use strict";
+var __defProp = Object.defineProperty;
+var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __hasOwnProp = Object.prototype.hasOwnProperty;
+var __export = (target, all) => {
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+var __copyProps = (to, from, except, desc) => {
+  if (from && typeof from === "object" || typeof from === "function") {
+    for (let key of __getOwnPropNames(from))
+      if (!__hasOwnProp.call(to, key) && key !== except)
+        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
+  }
+  return to;
+};
+var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
+
+// src/index.ts
+var index_exports = {};
+__export(index_exports, {
+  makeCorrelationId: () => makeCorrelationId,
+  makeEntityId: () => makeEntityId,
+  makeEventId: () => makeEventId,
+  makeSourceId: () => makeSourceId
+});
+module.exports = __toCommonJS(index_exports);
+function makeEventId(s) {
+  return s;
+}
+function makeEntityId(s) {
+  return s;
+}
+function makeSourceId(s) {
+  return s;
+}
+function makeCorrelationId(s) {
+  return s;
+}
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {
+  makeCorrelationId,
+  makeEntityId,
+  makeEventId,
+  makeSourceId
+});
---
+++ packages/openspec-events/dist/index.js
@@ -0,0 +1,19 @@
+// src/index.ts
+function makeEventId(s) {
+  return s;
+}
+function makeEntityId(s) {
+  return s;
+}
+function makeSourceId(s) {
+  return s;
+}
+function makeCorrelationId(s) {
+  return s;
+}
+export {
+  makeCorrelationId,
+  makeEntityId,
+  makeEventId,
+  makeSourceId
+};
